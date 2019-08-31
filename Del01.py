4463fc24d62053216b24da7f6a8d73ca1c32570c
NAYO OGILVIE <nathaniel.ogilvie@ibm.com>
2019-08-31 09:32:37 -0400

    Upto Step 61?ish

---
+++ Del01.py
@@ -0,0 +1,41 @@
+import sys
+#Add path to PATH
+sys.path.insert(0,'..')
+
+
+import Leap
+import random
+
+#Add classes and variables from other python files
+from pygameWindow import PYGAME_WINDOW
+
+
+#Variable Init
+pygameWindow = PYGAME_WINDOW()
+x = 300
+y = 300
+
+#Function List
+def Perturb_Circle_Position():
+    global x, y
+    fourSidedDieRoll = random.randint(1,4)
+    if (fourSidedDieRoll == 1):
+        x-=2
+    elif (fourSidedDieRoll == 2):
+        x+=2
+    elif (fourSidedDieRoll == 3):
+        y-=2
+    elif (fourSidedDieRoll == 4):
+        y+=2
+        
+
+#Infinite Loop
+while True:
+    #print('Draw Something.')
+    pygameWindow.Prepare()
+    pygameWindow.Draw_Black_Circle(x,y)
+    Perturb_Circle_Position()
+    pygameWindow.Reveal()
+
+
+print(pygameWindow)
