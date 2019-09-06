b3202c65514a2f087b3f93a09c2051a07c23090b
NAYO OGILVIE <nathaniel.ogilvie@ibm.com>
2019-09-06 10:34:34 -0400

    Starting Deliverable 2

---
+++ Del02.py
@@ -0,0 +1,97 @@
+import sys
+#Add path to PATH
+sys.path.insert(0,'../x86')
+
+
+import Leap
+import random
+
+#Add classes and variables from other python files
+from pygameWindow import PYGAME_WINDOW
+
+
+#Variable Init
+pygameWindow = PYGAME_WINDOW()
+x = 300
+y = 300
+pygameX = 300
+pygameY = 300
+xMin = 1000.0
+xMax = -1000.0
+yMin = 1000.0
+yMax = -1000.0
+
+#Initialize controller
+controller = Leap.Controller()
+
+#Function List
+def Perturb_Circle_Position():
+    global x, y
+    fourSidedDieRoll = random.randint(1,4)
+    if (fourSidedDieRoll == 1):
+        x-=2
+    elif (fourSidedDieRoll == 2):
+        x+=2
+    elif (fourSidedDieRoll == 3):
+        y-=2
+    elif (fourSidedDieRoll == 4):
+        y+=2
+
+def Scale(value, before_min, before_max, after_min, after_max):
+    if ((before_max - before_min) <= 0):
+        return 0
+    #print("Before min: ", before_min)
+    #print("Before max: ", before_max)
+    #print("value: ", value)
+    scaleValue = (float(value) - float(before_min)) / (float(before_max) - float(before_min))
+    scaledPointValue = (scaleValue * (after_max - after_min)) + after_min
+    #print(scaledPointValue)
+    return int(scaledPointValue)
+    
+def Handle_Frame (frame):
+    global x, y, xMin, xMax, yMin, yMax
+    #print(frame)
+    hand = frame.hands[0]
+    fingers = hand.fingers
+    #Leaving as 1 since that is the integer map and class can't be founds
+    indexFingerList = fingers.finger_type(1)
+    indexFinger = indexFingerList[0]
+    #Same as above making 3 
+    distalPhalanx = indexFinger.bone(3)
+    tip = distalPhalanx.next_joint
+    x = int(tip[0])
+    y = int(tip[1])
+    if ( x < xMin):
+        xMin = x
+    if ( x > xMax):
+        xMax = x
+    if ( y < yMin):
+        yMin = y
+    if ( y > yMax):
+        yMax = y
+    #print("xMin: ", xMin)
+    #print("xMax: ", xMax)
+    #print("yMin: ", yMin)
+    #print("yMax: ", yMax)
+    #print(tip)
+    #print(hand)
+    
+#Infinite Loop
+while True:
+    #print('Draw Something.')
+    #print(frame)
+    pygameWindow.Prepare()
+    frame = controller.frame()
+    hands = frame.hands
+    if (not hands.is_empty):
+        #print("hand detected.")
+        Handle_Frame(frame)
+        pygameX = Scale(x, xMin, xMax, 0, 600)
+        pygameY = Scale(y, yMin, yMax, 600, 0)
+    pygameWindow.Draw_Black_Circle(pygameX,pygameY)
+    #pygameWindow.Draw_Black_Circle(x,y)
+    #Perturb_Circle_Position()
+    pygameWindow.Reveal()
+
+
+print(pygameWindow)
